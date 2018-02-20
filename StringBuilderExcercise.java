4508804bad62a2073a385eeb4950527f6ebc0d1d
tomijava <grochowski.t91@gmail.com>
2018-02-20 23:02:36 +0100

    fixed StringBuilder

--- StringBuilderExcercise.java
+++ StringBuilderExcercise.java
@@ -3,27 +3,25 @@ import java.util.Scanner;
 public class StringBuilderExcercise {
 
 	public static void main(String[] args) {
-/*
-	Pobierz od użytkownika liczbę, która określać będzie ile wyrazów użytkownik chce wprowadzić. 
-	Następnie wczytaj od niego listę ciągów znaków na podstawie, których wygenerujesz wynik.
+	/*
+		Pobierz od użytkownika liczbę, która określać będzie ile wyrazów użytkownik chce wprowadzić. 
+		Następnie wczytaj od niego listę ciągów znaków na podstawie, których wygenerujesz wynik.
 
-	Wynikiem jest wyraz składający się z ostatnich liter każdego z wprowadzonych słów. 
-	Wykorzystaj klasy String i StringBuilder.
-*/	
-	StringBuilder builder = new StringBuilder();
-	Scanner scanner = new Scanner(System.in);
+		Wynikiem jest wyraz składający się z ostatnich liter każdego z wprowadzonych słów. 
+		Wykorzystaj klasy String i StringBuilder.
+	*/	
+		StringBuilder builder = new StringBuilder();
+		Scanner scanner = new Scanner(System.in);
 	
-	System.out.println("Number of words:");
-	int numberWords = scanner.nextInt();
-	scanner.nextLine();
-	
-	System.out.println("Give words");
-	
-		for(int i = 0; i<numberWords; i++)
+		System.out.println("Enter the number of words: ");
+		int wordsNumber = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Number of words: " + wordsNumber);
+		
+		for(int i = 1; i<=wordsNumber; i++)
 		{
-			System.out.println("Enter word");
+			System.out.println("Enter " + (i) + " word");
 			String word = scanner.nextLine();
-			System.out.println(word);
 			builder.append(word.charAt(word.length()-1));
 		}
 		System.out.println(builder.toString());
