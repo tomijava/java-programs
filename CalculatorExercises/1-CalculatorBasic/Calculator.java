0a3ac7e1ea07dbba9728a74b40cefb7625465237
tomijava <grochowski.t91@gmail.com>
2018-03-24 23:09:44 +0100

    add Calculator

--- CalculatorExercises/1-CalculatorBasic/Calculator.java
+++ CalculatorExercises/1-CalculatorBasic/Calculator.java
@@ -20,156 +20,80 @@ public class Calculator {
         }
     }
 
-    public static double operation (double score, double number, MathOperator operator) {
+    public static double calculation (double result, double number, MathOperator operator) {
         
-        double result = 0;
+       
         switch(operator) {
 
-            case SUM : result = score + number; break;
-            case SUBSTRACTION: result =  score - number; break;
-            case DIVIDE: result = score / number; break;
-            case MULTIPLCATION: result = score * number; break;
+            case SUM : result = result + number; break;
+            case SUBSTRACTION: result =  result - number; break;
+            case DIVIDE: result = result / number; break;
+            case MULTIPLCATION: result = result * number; break;
 
         }
         return result;
     } 
 
-
-
-
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
-        Double number = new Double(30);
-
-        List<Double> number2 = new ArrayList<Double>();
         
-        double result1 = 0; ;
-
-        List<String> history = new ArrayList<String>();
-
-        List<Character> sign2 = new ArrayList<Character>();
+        double result = 0;
 
         while(true) {
 
-            boolean cont = true;
+            System.out.println(result);
 
-            StringBuilder string = new StringBuilder("");
+            StringBuilder input = new StringBuilder(scan.nextLine());
 
-            StringBuilder string2 = new StringBuilder("");
+            Calculator.removeWhiteSpace(input);
 
-            System.out.println(result1);
+            StringBuilder stringNumber = new StringBuilder("");       
+      
+            try {
 
-            string.append(scan.nextLine());
-
-            Calculator.removeWhiteSpace(string);
-
-            if (string.toString().equals("exit")) {
-                break;
-            }
-
-            else if (string.toString().equals("show")) {
-
-                for (String equation : history) {
-
-                    System.out.println(equation);
-                }
-            }
-
-            else if (string.toString().equals("back")) {
-  
-                
-                if(history.size() == 0) {
-                    System.out.println("Nie ma zadnej operacji do cofniecia");
-                }        
-                else {
-                    System.out.print("Wycofywanie operacji ");
-                    System.out.println(history.get(history.size()-1));
-
-                    history.remove(history.size()-1);
-
-                    if(sign2.get(sign2.size()-1).charValue() == '+') {
-                        result1 -= number2.get(number2.size()-1).doubleValue();
-                    }
-                    else if (sign2.get(sign2.size()-1).charValue() == '-') {
-                        result1 += number2.get(number2.size()-1).doubleValue();
-                    }
-                    else if (sign2.get(sign2.size()-1).charValue() == '/') {
-                        result1 *= number2.get(number2.size()-1).doubleValue();
-                    }
-                    else if (sign2.get(sign2.size()-1).charValue() == '*') {
-                        result1 /= number2.get(number2.size()-1).doubleValue();
-                    }
-
-                    number2.remove(number2.size()-1);
-                    sign2.remove(sign2.size()-1);                        
-                }
-            }
-            else {
-
-                for (int i = 1; i< string.length(); i++) {
-
-                    if (!(string.charAt(i) >= '0' && string.charAt(i) <= '9' || string.charAt(i) == '.')) {
-
-                    System.out.println("Zle polecenie");
-                
-                    cont = false;
-
+                if (input.toString().equals("exit")) {
                     break;
-
-                    }
-
-                    else {  
-                        string2.append(string.charAt(i));
-                    }
                 }
 
-                try {
+                else {
 
-                number = number.parseDouble(string2.toString());;
+                    for (int i = 1; i< input.length(); i++) {
 
-                number2.add(number);
+                        if (!(input.charAt(i) >= '0' && input.charAt(i) <= '9' || input.charAt(i) == '.')) {
 
-                } catch (NumberFormatException e) {
-
-                    continue;
-                }
-
-                
-                if(cont) {
-
-                    switch (string.charAt(0)) {
-
-                    case '+':
-                        result1 = Calculator.operation(result1, number, MathOperator.SUM);
-                        history.add((result1 - number) + " + " + number + " = " + result1);
-                        sign2.add('+');
                         break;
 
+                        }
+
+                        else {  
+                            stringNumber.append(input.charAt(i));
+                        }
+                    }
+
+                    switch (input.charAt(0)) {
+
+                    case '+':
+                        result = Calculator.calculation(result, Double.parseDouble(stringNumber.toString()), MathOperator.SUM);
+                        break;
                     case '-':
-                        result1 = Calculator.operation(result1, number, MathOperator.SUBSTRACTION);
-                        history.add((result1 + number) + " - " + number + " = " + result1);
-                        sign2.add('-');
+                        result = Calculator.calculation(result, Double.parseDouble(stringNumber.toString()), MathOperator.SUBSTRACTION);
                         break;
 
                     case '/':
-                        result1 = Calculator.operation(result1, number, MathOperator.DIVIDE);
-                        history.add((result1 * number) + " / " + number + " = " + result1);
-                        sign2.add('/');
+                        result = Calculator.calculation(result, Double.parseDouble(stringNumber.toString()), MathOperator.DIVIDE);
                         break;
-
                     case '*':
-                        result1 = Calculator.operation(result1, number, MathOperator.MULTIPLCATION);
-                        history.add((result1 / number) + " * " + number + " = " + result1);
-                        sign2.add('*');
+                        result = Calculator.calculation(result, Double.parseDouble(stringNumber.toString()), MathOperator.MULTIPLCATION);
                         break;
-
-                    default:
+                    default : 
+                        System.out.println("Złe polecenie");
+                    }
+                } 
+            } catch (StringIndexOutOfBoundsException err) {
                         System.out.println("Zle polecenie");
-                        break;
-                    }                   
-                }
+            } catch (NumberFormatException err2) {
+                        System.out.println("Zle Polecenie");                           
             }
         }
     }
