7aca590c2fbff2cc264855520791b6cd3e71d8d4
tomijava <t.grochowski91@gmail.com>
2018-04-14 20:52:24 +0200

    delated exceptions, added reges

--- calculator/interfaces/Main.java
+++ calculator/interfaces/Main.java
@@ -1,85 +1,86 @@
 package calculator.interfaces;
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.regex.*;
 
 public class Main {
+
+    public static String basicCalculator = "1";
+    public static String moduloCalculator = "2";
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String calculatorModel = "0";
+        Pattern pattern = Pattern.compile("[\\+\\-\\*\\/]{1}[0-9]+");
 
-        closeProgram: 
-            while(true) {
-                System.out.println("Please chose calculator model");
-                System.out.println("Enter 1 for basic calculator");
-                System.out.println("Enter 2 for modulo calculator");
+        System.out.println("Please chose calculator model");
+        System.out.println("Enter 1 for basic calculator");
+        System.out.println("Enter 2 for modulo calculator");
                     
-                calculatorModel = scan.nextLine();
+        calculatorModel = scan.nextLine();
 
-                if (calculatorModel.equals("1")) {
-                    Calculator calculatorBasic = new CalculatorBasic();
-                    while(true) {
-                        System.out.println(calculatorBasic.toString());
-                        String input = StringHelper.removeWhitespace(scan.nextLine());
-                        try {
-                            if (input.equals("exit")) {
-                                break closeProgram;  
-                            }
-                            switch (input.charAt(0)) {
-                                case '+' :
-                                    calculatorBasic.add(Double.parseDouble(input.substring(1)));
-                                    break;
-                                case  '-' :
-                                    calculatorBasic.subtract(Double.parseDouble(input.substring(1)));
-                                    break;
-                                case '/':
-                                    calculatorBasic.divide(Double.parseDouble(input.substring(1)));
-                                    break;
-                                case '*':
-                                    calculatorBasic.multiply(Double.parseDouble(input.substring(1)));
-                                    break;
-                                default:
-                                    System.out.println("Invalid command");
-                            }
-                        } catch (Exception exceptions) {
-                            System.out.println("Invalid command");
-                        }
-                    }
+        if (calculatorModel.equals(basicCalculator)) {
+            Calculator calculatorBasic = new CalculatorBasic();
+            while(true) {
+                System.out.println(calculatorBasic.toString());
+                String input = StringHelper.removeWhitespace(scan.nextLine());
+                Matcher matcher = pattern.matcher(input);
+                        
+                if (input.equals("exit")) {
+                    break;  
                 }
-                else if (calculatorModel.equals("2")) {
-                    System.out.println("Enter modulo number");
-                    Calculator calculatorModulo = new CalculatorModulo(scan.nextInt());
-                    scan.nextLine();
-                    while(true) {
-                        System.out.println(calculatorModulo.toString());
-                        String input = StringHelper.removeWhitespace(scan.nextLine());
-                        try {
-                            if (input.equals("exit")) {
-                                break closeProgram;  
-                            }
-                            switch (input.charAt(0)) {
-                                case '+':
-                                    calculatorModulo.add(Double.parseDouble(input.substring(1)));
-                                    break;
-                                case '-':
-                                    calculatorModulo.subtract(Double.parseDouble(input.substring(1)));
-                                    break;
-                                case '/':
-                                    calculatorModulo.divide(Double.parseDouble(input.substring(1)));
-                                    break;
-                                case '*':
-                                    calculatorModulo.multiply(Double.parseDouble(input.substring(1)));
-                                    break;
-                                default:
-                                    System.out.println("Invalid command");
-                            }
-                        } catch (Exception exceptions) {
-                            System.out.println("Invalid command");
-                        }
+                else if(matcher.matches()) {
+                    switch (input.charAt(0)) {
+                        case '+' :
+                            calculatorBasic.add(Double.parseDouble(input.substring(1)));
+                            break;
+                        case  '-' :
+                            calculatorBasic.subtract(Double.parseDouble(input.substring(1)));
+                            break;
+                        case '/':
+                            calculatorBasic.divide(Double.parseDouble(input.substring(1)));
+                            break;
+                        case '*':
+                            calculatorBasic.multiply(Double.parseDouble(input.substring(1)));
+                            break;
+                    }       
+                }
+                else {
+                    System.out.println("Bad command");
+                }
+            }
+        }
+        else if (calculatorModel.equals(moduloCalculator)) {
+            System.out.println("Enter modulo number");
+            Calculator calculatorModulo = new CalculatorModulo(scan.nextInt());
+            scan.nextLine();
+            while(true) {
+                System.out.println(calculatorModulo.toString());
+                String input = StringHelper.removeWhitespace(scan.nextLine());
+                Matcher matcher = pattern.matcher(input);
+                        
+                if (input.equals("exit")) {
+                    break;  
+                }
+                else if(matcher.matches()) {   
+                    switch (input.charAt(0)) {
+                        case '+':
+                            calculatorModulo.add(Double.parseDouble(input.substring(1)));
+                            break;
+                        case '-':
+                            calculatorModulo.subtract(Double.parseDouble(input.substring(1)));
+                            break;
+                        case '/':
+                            calculatorModulo.divide(Double.parseDouble(input.substring(1)));
+                            break;
+                        case '*':
+                            calculatorModulo.multiply(Double.parseDouble(input.substring(1)));
+                            break;
                     }
                 }
                 else {
-                    System.out.println("Invalid command");
+                    System.out.println("Bad commnad");
+                }
             }
         }
     }
