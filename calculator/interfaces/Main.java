0e47e18e0ff8a3c98730d4b8ef23875149a98ec8
tomijava <grochowski.t91@gmail.com>
2018-04-15 22:00:46 +0200

    refacotr Main

--- calculator/interfaces/Main.java
+++ calculator/interfaces/Main.java
@@ -9,80 +9,69 @@ public class Main {
     public static String moduloCalculator = "2";
 
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String calculatorModel = "0";
         Pattern pattern = Pattern.compile("[\\+\\-\\*\\/]{1}[0-9]+");
+        Calculator calculator; 
 
         System.out.println("Please chose calculator model");
         System.out.println("Enter 1 for basic calculator");
         System.out.println("Enter 2 for modulo calculator");
-                    
+
         calculatorModel = scan.nextLine();
 
-        if (calculatorModel.equals(basicCalculator)) {
-            Calculator calculatorBasic = new CalculatorBasic();
-            while(true) {
-                System.out.println(calculatorBasic.toString());
-                String input = StringHelper.removeWhitespace(scan.nextLine());
-                Matcher matcher = pattern.matcher(input);
+
+
+        while(true) {
+            if (calculatorModel.equals(basicCalculator)) {
+                calculator = new CalculatorBasic();
+                break;
+            }
+            else if (calculatorModel.equals(moduloCalculator)) {
+
+                System.out.println("Enter modulo number");
+                calculator = new CalculatorModulo(scan.nextInt());
+                scan.nextLine();
+                break;
+            }
+            else {
+                System.out.println("Bad command");
+            }
+        }
+
+        
+
+        while(true) {
+
+            System.out.println(calculator.toString());
+            String input = StringHelper.removeWhitespace(scan.nextLine());
+            Matcher matcher = pattern.matcher(input);
                         
-                if (input.equals("exit")) {
-                    break;  
-                }
-                else if(matcher.matches()) {
-                    switch (input.charAt(0)) {
-                        case '+' :
-                            calculatorBasic.add(Double.parseDouble(input.substring(1)));
-                            break;
-                        case  '-' :
-                            calculatorBasic.subtract(Double.parseDouble(input.substring(1)));
-                            break;
-                        case '/':
-                            calculatorBasic.divide(Double.parseDouble(input.substring(1)));
-                            break;
-                        case '*':
-                            calculatorBasic.multiply(Double.parseDouble(input.substring(1)));
-                            break;
+            if (input.equals("exit")) {
+                break;  
+            }
+            else if(matcher.matches()) {
+                switch (input.charAt(0)) {
+                    case '+' :
+                        calculator.add(Double.parseDouble(input.substring(1)));
+                        break;
+                    case  '-' :
+                        calculator.subtract(Double.parseDouble(input.substring(1)));
+                        break;
+                    case '/':
+                        calculator.divide(Double.parseDouble(input.substring(1)));
+                        break;
+                    case '*':
+                        calculator.multiply(Double.parseDouble(input.substring(1)));
+                        break;
                     }       
-                }
-                else {
-                    System.out.println("Bad command");
-                }
+            }
+            else {
+                System.out.println("Bad command");
             }
         }
-        else if (calculatorModel.equals(moduloCalculator)) {
-            System.out.println("Enter modulo number");
-            Calculator calculatorModulo = new CalculatorModulo(scan.nextInt());
-            scan.nextLine();
-            while(true) {
-                System.out.println(calculatorModulo.toString());
-                String input = StringHelper.removeWhitespace(scan.nextLine());
-                Matcher matcher = pattern.matcher(input);
-                        
-                if (input.equals("exit")) {
-                    break;  
-                }
-                else if(matcher.matches()) {   
-                    switch (input.charAt(0)) {
-                        case '+':
-                            calculatorModulo.add(Double.parseDouble(input.substring(1)));
-                            break;
-                        case '-':
-                            calculatorModulo.subtract(Double.parseDouble(input.substring(1)));
-                            break;
-                        case '/':
-                            calculatorModulo.divide(Double.parseDouble(input.substring(1)));
-                            break;
-                        case '*':
-                            calculatorModulo.multiply(Double.parseDouble(input.substring(1)));
-                            break;
-                    }
-                }
-                else {
-                    System.out.println("Bad commnad");
-                }
-            }
-        }
+        scan.close();
     }
 }
  
