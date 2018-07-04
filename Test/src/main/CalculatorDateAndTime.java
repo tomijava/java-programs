2ff2c70e5795b6daff401dd247fc631a5cdbecd1
tomijava <t.grochowski91@gmail.com>
2018-07-04 22:01:24 +0200

    adding DateTimeCalculator design and test

---
+++ Test/src/main/CalculatorDateAndTime.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CalculatorDateAndTime {
+
+    public static String toString(ZonedDateTime date) { return "0";}
+    public static ZonedDateTime calculate(ZonedDateTime date, String operation) {return null;}
+    public static String showSecondSince(ZonedDateTime date, String input) {return "";}
+    public static String whenSunday (ZonedDateTime date) {return null;}
+}
+
+
